c9e17180cdfbe06923666cf1e8af79044c786c44
Zan Rocha <andre.rocha2302@gmail.com>
2025-05-07 19:09:16 -0300

    feat: For1

---
+++ exercicios/src/controle/For1.java
@@ -0,0 +1,9 @@
+package controle;
+
+public class For1 {
+    public static void main(String[] args) {
+        for( int i = 1; i <= 10; i++ ){
+            System.out.println("Bom dia " + i + " vezes");
+        }
+    }
+}
