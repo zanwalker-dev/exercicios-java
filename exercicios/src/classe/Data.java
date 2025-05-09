8e9debb19c90ba19e8963bae4f2a91d084a48891
Zan Rocha <andre.rocha2302@gmail.com>
2025-05-09 18:59:59 -0300

    feat: Data Constructor default and with params

--- exercicios/src/classe/Data.java
+++ exercicios/src/classe/Data.java
@@ -6,6 +6,18 @@ public class Data {
     String mes;
     String ano;
 
+    Data(){
+        dia = "01";
+        mes = "01";
+        ano = "1970";
+    }
+
+    Data(String dia, String mes, String ano){
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+
     String obterDataFormatada(){
         return dia + "/" + mes + "/" + ano;
     }
--- exercicios/src/classe/DataTeste.java
+++ exercicios/src/classe/DataTeste.java
@@ -2,18 +2,19 @@ package classe;
 
 public class DataTeste {
     public static void main(String[] args) {
-        Data d1 = new Data();
-        d1.dia = "23";
-        d1.mes = "02";
-        d1.ano = "1997";
 
-        Data d2 = new Data();
-        d2.dia = "27";
-        d2.mes = "12";
-        d2.ano = "1993";
+        Data d1 = new Data(); //Exemplo constructor padrão
+
+        Data d2 = new Data( "23","02","1997" ); //Exemplo passando parametros
+
+        Data d3 = new Data(); //Exemplo padrão mas alterando manualmente
+        d3.dia = "27";
+        d3.mes = "12";
+        d3.ano = "1993";
 
         System.out.println(d1.obterDataFormatada());
         System.out.println(d2.obterDataFormatada());
+        System.out.println(d3.obterDataFormatada());
 
 
     }
