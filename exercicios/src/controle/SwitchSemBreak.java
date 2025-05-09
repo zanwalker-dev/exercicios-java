9dae2487e452eeb2aec9bfdfaf6d95a35110c692
Zan Rocha <andre.rocha2302@gmail.com>
2025-05-09 07:05:14 -0300

    feat: SwitchSemBreak

---
+++ exercicios/src/controle/SwitchSemBreak.java
@@ -0,0 +1,27 @@
+package controle;
+
+public class SwitchSemBreak {
+    public static void main(String[] args) {
+
+        String faixa = "preta";
+
+        switch(faixa){
+            case "preta":
+                System.out.println("Sei fazer o Bassai-Dai...");
+            case "marrom":
+                System.out.println("Sei fazer o Tekki Shodan");
+            case "roxa":
+                System.out.println("Sei fazer o Heian Godan");
+            case "verde":
+                System.out.println("Sei fazer o Heian Yodan");
+            case "laranja":
+                System.out.println("Sei fazer o Heian Sandan");
+            case "vermelha":
+                System.out.println("Sei fazer o Heian Nidan");
+            case "amarela":
+                System.out.println("Sei fazer o Heian Shodan");
+            default:
+                System.out.println("Sei lutar karatê");
+        }
+    }
+}
