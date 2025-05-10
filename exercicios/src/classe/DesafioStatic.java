2a8a3cf1508eca18f24f0150f8e8280853c49b8e
Zan Rocha <andre.rocha2302@gmail.com>
2025-05-10 10:49:27 -0300

    commit: DesafioStatic

--- exercicios/src/classe/DesafioStatic.java
+++ exercicios/src/classe/DesafioStatic.java
@@ -4,7 +4,7 @@ public class DesafioStatic {
 
     int a = 3;//não pode alterar essa linha
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { //static só pode acessar outro static
 
        DesafioStatic d = new DesafioStatic(); //para acessar um objeto de instancia dentro da classe preciso instanciar
         System.out.println(d.a);
