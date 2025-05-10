5a29c3b1fd6f68e33e476c169cd2e9f30db0a2b6
Zan Rocha <andre.rocha2302@gmail.com>
2025-05-10 10:48:17 -0300

    feat: DesafioStatic

---
+++ exercicios/src/classe/DesafioStatic.java
@@ -0,0 +1,12 @@
+package classe;
+
+public class DesafioStatic {
+
+    int a = 3;//não pode alterar essa linha
+
+    public static void main(String[] args) {
+
+       DesafioStatic d = new DesafioStatic(); //para acessar um objeto de instancia dentro da classe preciso instanciar
+        System.out.println(d.a);
+    }
+}
