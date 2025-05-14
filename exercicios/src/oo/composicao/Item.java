2f94a26e39ffc1121932b6c799da818878a8c1f8
Zan Rocha <andre.rocha2302@gmail.com>
2025-05-14 12:42:45 -0300

    modified: Constructor Item with params

--- exercicios/src/oo/composicao/Item.java
+++ exercicios/src/oo/composicao/Item.java
@@ -5,4 +5,10 @@ public class Item {
     String nome;
     int quantidade;
     double preco;
+
+    Item(String nome, int quantidade, double preco) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.preco = preco;
+    }
 }
