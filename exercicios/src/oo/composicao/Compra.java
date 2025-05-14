b178dafd052817518cadb37faa7710e8ec1bbb25
Zan Rocha <andre.rocha2302@gmail.com>
2025-05-14 12:40:20 -0300

    feat: Compra and Item

---
+++ exercicios/src/oo/composicao/Compra.java
@@ -0,0 +1,11 @@
+package oo.composicao;
+
+import java.util.ArrayList;
+
+public class Compra {
+
+    String nomeCliente;
+    ArrayList<Item> itens = new ArrayList<>();
+
+
+}
---
+++ exercicios/src/oo/composicao/Item.java
@@ -0,0 +1,8 @@
+package oo.composicao;
+
+public class Item {
+
+    String nome;
+    int quantidade;
+    double preco;
+}
