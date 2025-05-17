6ba5d053ce572922e557c1a721dcd2f41ea1dd1b
Zan Rocha <andre.rocha2302@gmail.com>
2025-05-17 09:02:48 -0300

    update: optimized version done

--- exercicios/src/lambdas/Desafio.java
+++ exercicios/src/lambdas/Desafio.java
@@ -1,5 +1,6 @@
 package lambdas;
 
+import java.util.Locale;
 import java.util.function.Function;
 import java.util.function.UnaryOperator;
 import java.math.BigDecimal;
@@ -18,34 +19,55 @@ public class Desafio {
         5- Formatar: R$1234,56
          */
 
-        double imposto = 0.085;
+        // ------------------------------------------ MINHA SOLUÇÃ0 ---------------------------------------------
 
-        UnaryOperator<Double> precoReal = precoCheio ->  precoCheio * (1 - p.getDesconto());
-        UnaryOperator<Double> precoImposto = precoCheio -> {
-            if(precoCheio >= 2500){
-                return precoCheio = precoCheio * (1 + imposto);
-            } else {
-               return precoCheio;
-            }
-        };
-        UnaryOperator<Double> precoFrete = precoCheio -> {
-            if(precoCheio >= 3000){
-                return precoCheio += 100;
-            } else {
-                return precoCheio += 50;
-            }
-        };
-        UnaryOperator<Double> precoArredondado = precoCheio -> {
-            BigDecimal bd = new BigDecimal(precoCheio);
-            bd = bd.setScale(2, RoundingMode.HALF_UP);
-            return bd.doubleValue();
-        };
-        Function<Double, String> precoFinal = valor -> "R$" + valor;
+//        double imposto = 0.085;
+//
+//        UnaryOperator<Double> precoReal = precoCheio ->  precoCheio * (1 - p.getDesconto());
+//        UnaryOperator<Double> precoImposto = precoCheio -> {
+//            if(precoCheio >= 2500){
+//                return precoCheio = precoCheio * (1 + imposto);
+//            } else {
+//               return precoCheio;
+//            }
+//        };
+//        UnaryOperator<Double> precoFrete = precoCheio -> {
+//            if(precoCheio >= 3000){
+//                return precoCheio += 100;
+//            } else {
+//                return precoCheio += 50;
+//            }
+//        };
+//        UnaryOperator<Double> precoArredondado = precoCheio -> {
+//            BigDecimal bd = new BigDecimal(precoCheio);
+//            bd = bd.setScale(2, RoundingMode.HALF_UP);
+//            return bd.doubleValue();
+//        };
+//        Function<Double, String> precoFinal = valor -> "R$" + valor;
+//
+//        System.out.println(p.getPreco());
+//        System.out.println(precoReal
+//                .andThen(precoImposto).andThen(precoFrete)
+//                .andThen(precoArredondado).andThen(precoFinal)
+//                .apply(p.getPreco()));
 
-        System.out.println(p.getPreco());
-        System.out.println(precoReal
-                .andThen(precoImposto).andThen(precoFrete)
-                .andThen(precoArredondado).andThen(precoFinal)
-                .apply(p.getPreco()));
+        // ------------------------------------------ SOLUÇÃ0 OTIMIZADA  ---------------------------------------------
+
+        Function<Produto, Double> precoFinal =
+                prod -> prod.preco *(1 - prod.desconto);
+        UnaryOperator<Double> impostoMunicipal =
+                valor -> valor >= 2500 ? valor * 1.085 : valor;
+        UnaryOperator<Double> frete =
+                valor -> valor >= 3000 ? valor + 100 : valor + 50;
+        UnaryOperator<Double> arredondar =
+                valor -> Double.parseDouble(String.format(Locale.US, "%.2f", valor));
+        Function<Double, String> formatar =
+                valor -> ("R$" + valor).replace(".", ",");
+
+        String preco = precoFinal
+                .andThen(impostoMunicipal).andThen(frete)
+                .andThen(arredondar).andThen(formatar).apply(p);
+
+        System.out.println(preco);
     }
 }
