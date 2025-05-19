012c4ee1b9e3c85330d9313243af2ba479a0678c
Zan Rocha <andre.rocha2302@gmail.com>
2025-05-19 16:19:54 -0300

    feat: hashcode and equals created

--- exercicios/src/streams/Aluno.java
+++ exercicios/src/streams/Aluno.java
@@ -1,5 +1,7 @@
 package streams;
 
+import java.util.Objects;
+
 public class Aluno {
     final String nome;
     final double nota;
@@ -12,5 +14,17 @@ public class Aluno {
     @Override
     public String toString() {
         return nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Aluno aluno = (Aluno) o;
+        return Double.compare(nota, aluno.nota) == 0 && Objects.equals(nome, aluno.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, nota);
     }
 }
