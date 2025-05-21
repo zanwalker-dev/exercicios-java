9eb877169738624ad34ad6d08ace543882cf7afa
Zan Rocha <andre.rocha2302@gmail.com>
2025-05-21 10:00:51 -0300

    update: Aluno attributes now is public

--- exercicios/src/excessao/Aluno.java
+++ exercicios/src/excessao/Aluno.java
@@ -3,8 +3,8 @@ package excessao;
 import java.util.Objects;
 
 public class Aluno {
-    final String nome;
-    final double nota;
+    public final String nome;
+    public final double nota;
 
     public Aluno(String nome, double nota) {
         this.nome = nome;
