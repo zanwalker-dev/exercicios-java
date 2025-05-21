f540c6a407bac7c5de58e9724d2feee638d50b8c
Zan Rocha <andre.rocha2302@gmail.com>
2025-05-21 10:01:39 -0300

    feat: 2 customized exception for using has example

---
+++ exercicios/src/excessao/personalizada/NumeroNegativoException.java
@@ -0,0 +1,13 @@
+package excessao.personalizada;
+
+public class NumeroNegativoException extends RuntimeException {
+
+    private String nomeDoAtributo;
+    NumeroNegativoException( String nomeDoAtributo) {
+        this.nomeDoAtributo = nomeDoAtributo;
+    }
+
+    public String getMessage(){
+        return String.format("O atributo %s está negativo.", nomeDoAtributo);
+    }
+}
---
+++ exercicios/src/excessao/personalizada/StringVaziaException.java
@@ -0,0 +1,13 @@
+package excessao.personalizada;
+
+public class StringVaziaException extends RuntimeException {
+
+    private String nomeDoAtributo;
+    StringVaziaException( String nomeDoAtributo) {
+        this.nomeDoAtributo = nomeDoAtributo;
+    }
+
+    public String getMessage(){
+        return String.format("O atributo %s está vazio.", nomeDoAtributo);
+    }
+}
