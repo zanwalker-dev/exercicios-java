c8a89b3492dfb03d3db471d3da62d19a28fdff75
Zan Rocha <andre.rocha2302@gmail.com>
2025-06-03 12:15:45 -0300

    feat: insert four states to states table

---
+++ exercicios/src/bancoDeDadosRelacional/inserirEstados1.sql
@@ -0,0 +1,11 @@
+INSERT INTO estados
+    (nome, sigla, regial, populacao)
+VALUES
+    ('Acre', 'AC', 'Norte', 0.83);
+
+INSERT INTO estados
+    (nome, sigla, regial, populacao)
+VALUES
+    ('Alagoas', 'AL', 'Nordeste', 3.38),
+    ('Amapá', 'AP', 'Norte', 0.8),
+    ('Amazônas', 'AM', 'Norte', 4.06);
