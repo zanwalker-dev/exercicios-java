8651f09d1001947878b5224b429f3fa7c65fbda0
Zan Rocha <andre.rocha2302@gmail.com>
2025-06-03 13:05:31 -0300

    feat: testing UPDATE and updating Paraná and Maranhão registers

---
+++ exercicios/src/bancoDeDadosRelacional/atualizarEstados.sql
@@ -0,0 +1,9 @@
+-- Atualiza um registro, IMPORTANTE sempre usar o WHERE
+-- caso contrario vai atualizar TODOS os registros do banco
+UPDATE estados
+SET nome = 'Maranhão'
+WHERE sigla = 'MA';
+
+UPDATE estados
+SET nome = 'Paraná', populacao = 11.32
+WHERE sigla = 'PR';
